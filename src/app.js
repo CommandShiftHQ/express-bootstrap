075a0567e98f5f50b6fa16a9d8c085e320ba5d81
SanMasood <sanamasood.giki@gmail.com>
2020-04-30 07:59:26 +0100

    Jokes controllers and test modules; not passing

--- src/app.js
+++ src/app.js
@@ -1,8 +1,17 @@
 const express = require('express');
-const mainController = require('./controllers.js')
+const mainController = require('./controllers.js');
+const jokesController = require('./controllers.js');
+const randomController = require('./controllers.js');
+const random2Controller = require('./controllers.js');
 
 const app = express();
 
 app.get('/', mainController);
+
+app.get('/jokes', jokesController);
+
+app.get('/jokes/random', randomController);
+
+app.get('/joke/random/personal/:first/:last', random2Controller);
 
 module.exports = app;
--- src/controllers.js
+++ src/controllers.js
@@ -1,8 +1,22 @@
-
 const mainController = (req, res) => {
   res.send({
     message: 'Welcome to my Jokes API!',
   });
 };
+const jokesController = (req, res) => {
+  res.send({
+    message: 'This is the all jokes endpoint!',
+  });
+};
+const randomController = (req, res) => {
+  res.send({
+    message: 'This is the all jokes endpoint!',
+  });
+};
+const random2Controller = (req, res) => {
+  res.send({
+    message: 'This is the all jokes endpoint!',
+  });
+};
 
-module.exports = mainController;
+module.exports = mainController, jokesController, randomController,random2Controller;
