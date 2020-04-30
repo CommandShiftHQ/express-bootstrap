701d09b77a2181db401c79c5a70e8bc673043bdc
SanMasood <sanamasood.giki@gmail.com>
2020-04-30 18:27:26 +0100

    Parsed response made me LOL so worth a commit

--- src/controllers.js
+++ src/controllers.js
@@ -1,12 +1,26 @@
+const request = require('request');
+
 const mainController = (req, res) => {
   res.send({
     message: 'Welcome to my Jokes API!',
   });
 };
-const jokesController = (req, res) => {
+
+/* const jokesController = (req, res) => {
   res.send({
     message: 'This is the all jokes endpoint!',
   });
-};
+}; */
 
-module.exports = {mainController, jokesController};
+const jokesController = (req, res) => {
+  request('https://api.icndb.com/jokes', (error, jokesApiResponse) => {
+    if (error) {
+      console.log(error);
+    }
+    const parsedResponse = JSON.parse(jokesApiResponse.body);
+    res.send({ jokes: parsedResponse.value });
+    console.log(parsedResponse);
+  });
+  //
+};
+module.exports = { mainController, jokesController };
